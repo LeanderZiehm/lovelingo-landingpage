fb2ae2c7332ce93f2c0f7c69c1d298548806e77f
Leander <leander-arun.ziehm@stud.th-deg.de>
2025-07-02 10:14:25 +0200

    make supabase work

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/api/waitlist', formLimiter, waitlistRoutes);
 
+
 // Serve the landing page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
